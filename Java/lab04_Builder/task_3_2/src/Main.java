cc36a045ebbe3ac1129e658a7f48fd61f8a0b345
OleksiiKH0240 <homenko0240@gmail.com>
2024-03-20 23:34:54 +0200

    lab04_Builder_task_3_2_solution

--- Java/lab04_Builder/task_3_2/src/Main.java
+++ Java/lab04_Builder/task_3_2/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        MyStringBuilder myStringBuilder = new MyStringBuilder();
+        MyString myString = myStringBuilder
+                .append("Hello ")
+                .append("World")
+                .append(".")
+                .incert("Windows", 6)
+                .incert(" ", 13)
+                .replace("Admin", 14, 5)
+                .deleteSubString(5, 8)
+                .build();
+        System.out.println(myString);
     }
 }
